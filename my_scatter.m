bde5993cf4acbb6a6c85836dba3f7d0ea22b4804
LockyWebb <lockywebb@hotmail.com>
2020-12-03 14:10:11 +1000

    added second plot

--- my_scatter.m
+++ my_scatter.m
@@ -6,8 +6,12 @@
 x = linspace(0,3*pi,200);
 y = cos(x) + rand(1,200);
 
-x = y % Delete this 
+x = y; % Delete this 
+y2 = sin(x) * cos(x);
 sz = 25;
-c = linspace(1,10,length(x));
+c = linspace(1,5,length(x));
 scatter(x,y,sz,c,'filled')
 title('my scatter plot')
+
+plot(x, y2)
+title('second plot')
